e39e6d166ffe5be4fc42ada8158ac71b85e20dbb
Jay Bailey <jaybaileycs@gmail.com>
2026-04-16 06:01:38 +0000

    EVMBench

---
+++ src/evmbench/__init__.py
@@ -0,0 +1,9 @@
+"""EVMbench: Evaluating AI Agents on Smart Contract Security."""
+
+from evmbench.tasks import evmbench_detect, evmbench_exploit, evmbench_patch
+
+__all__ = [
+    "evmbench_detect",
+    "evmbench_exploit",
+    "evmbench_patch",
+]
